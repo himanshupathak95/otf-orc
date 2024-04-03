59089f90c776217cb14d6c144a0cdf3a0382a5aa
Himanshu Pathak <himanshupat95@gmail.com>
2024-04-03 14:27:59 +0530

    clearing roadmap, adding fileupload

---
+++ src/components/FileUpload.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+``
+const FileUpload = () => {
+  const [highlight, setHighlight] = useState(false);
+  const [preview, setPreview] = useState("");
+  const [drop, setDrop] = useState(false);
+  const [fileUploaded, setFileUploaded] = useState(false); 
+  const [responseData, setResponseData] = useState("");
+
+  const handleEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("enter!");
+    (preview === "") && setHighlight(true);
+  };
+
+  const handleOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("over!");
+    (preview === "") && setHighlight(true);
+  };
+
+  const handleLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("leave!");
+    setHighlight(false);
+  };
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("drop!");
+    setHighlight(false);
+    setDrop(true);
+
+    const [file] = e.target.files || e.dataTransfer.files;
+
+    uploadFile(file);
+    setFileUploaded(true);
+  };
+
+  function uploadFile(file) {
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+
+    reader.onload = () => {
+      // this is the base64 data
+      const fileRes = btoa(reader.result);
+      console.log(`data:image/jpg;base64,${fileRes}`);
+      setPreview(`data:image/jpg;base64,${fileRes}`);
+
+      // Create a new FormData object
+      const formData = new FormData();
+      // Append the file
+      console.log(file);
+      formData.append('file', file);
+
+      // Make a POST request to your backend
+      fetch('http://localhost:3000/upload', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response =>{
+        console.log(response);
+        return response.json()
+      })
+      .then(data => {
+        console.log(data);
+        setResponseData(data.data); 
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    };
+
+    reader.onerror = () => {
+      console.log("There is a problem while uploading...");
+    };
+  }
+
+  return (
+    <>
+      <div
+        onDragEnter={(e) => handleEnter(e)}
+        onDragLeave={(e) => handleLeave(e)}
+        onDragOver={(e) => handleOver(e)}
+        onDrop={(e) => handleUpload(e)}
+        // className={`upload${highlight ? " is-highlight" : drop ? " is-drop" : ""}`}
+        className="flex items-center justify-center w-72 h-72 text-white font-['Montserrat'] bg-blue-500 rounded-lg"
+        style={{ backgroundImage: `url(${preview})` }}
+      >
+        <form className="form">
+          <p>Drag and Drop image here</p>
+          <div 
+            // className="upload-button-wrap"
+            className="upload-button-wrap absolute bottom-0 left-0 w-52"
+          >
+            <input
+              type="file"
+              // className="upload-file"
+              className="opacity-0 w-full h-full absolute bottom-0 left-0" 
+              accept="image/*"
+              onChange={(e) => handleUpload(e)}
+            />
+            { !fileUploaded && <button className="flex items-center justify-center mt-2 w-full h-12 text-sm font-bold uppercase text-white bg-black" >FileUpload Here</button>}
+          </div>
+        </form>
+      </div>
+      <p>{responseData}</p>
+    </>
+  );
+};
+
+export default FileUpload;
