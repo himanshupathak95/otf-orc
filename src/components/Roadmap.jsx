94103c60ca919b09f9ebd32b68e86df54cc9e607
Himanshu Pathak <himanshupat95@gmail.com>
2024-04-03 21:39:30 +0530

    better-api

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -11,14 +11,9 @@ const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
     <div className="container md:pb-10">
       <Heading tag="Ready to get started" title="What we’re working on" />
-
       <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
         <FileUpload />
         <Gradient />
-      </div>
-
-      <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
-        <Button href="/roadmap">Our roadmap</Button>
       </div>
     </div>
   </Section>
