b6e6964fa96d57d81a12e5c41ef0ab95c15bcbde
Himanshu Pathak <himanshupat95@gmail.com>
2024-04-06 13:15:49 +0530

    Drag and drop area

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import RadialProgressBar from "./RadialProgressBar";
 import Alternatives from "./Alternatives";
 import Paragraphs from "./Paragraphs";
+import DragAndDropArea from "./DragAndDropArea";
 
 const FileUpload = () => {
   const [highlight, setHighlight] = useState(false);
@@ -89,44 +90,23 @@ const FileUpload = () => {
 
   return (
     <>
-      <div
-        onDragEnter={(e) => handleEnter(e)}
-        onDragLeave={(e) => handleLeave(e)}
-        onDragOver={(e) => handleOver(e)}
-        onDrop={(e) => handleUpload(e)}
-        className={`flex items-center justify-center w-98 h-[75vh] text-white  bg-transparent rounded-3xl relative overflow-hidden outline outline-1 outline-white/20 outline-offset-4
-                  ${highlight ? "bg-blue-500/50" : ""} 
-                  ${drop ? "opacity-70" : ""}`}
-      >
-        {preview && (
-          <img
-            src={preview}
-            alt="Uploaded preview"
-            className="absolute inset-0 object-cover w-full h-full"
-          />
-        )}
-        <form className="form">
-          <p className={`text-sm text-white ${drop ? "opacity-0" : ""}`}>
-            Drag and Drop image here
-          </p>
-          <div className="upload-button-wrap absolute bottom-0 left-0 w-52">
-            <input
-              type="file"
-              className="opacity-0 w-full h-full absolute bottom-0 left-0 cursor-pointer"
-              accept="image/*"
-              onChange={(e) => handleUpload(e)}
-            />
-            {!fileUploaded && (
-              <button className="flex items-center justify-center mt-2 w-full h-12 text-sm   text-white outline outline-1 outline-white/20 rounded-lg outline-offset-4">
-                Click to upload
-              </button>
-            )}
-          </div>
-        </form>
-      </div>
-      <Paragraphs showInstructions={showInstructions} responseData={responseData} isLoading={isLoading}/>
-      <RadialProgressBar responseData={responseData} isLoading={isLoading}/>
-      <Alternatives responseData={responseData} isLoading={isLoading}/>
+      <DragAndDropArea
+        handleEnter={handleEnter}
+        handleLeave={handleLeave}
+        handleOver={handleOver}
+        handleUpload={handleUpload}
+        highlight={highlight}
+        drop={drop}
+        preview={preview}
+        fileUploaded={fileUploaded}
+      />
+      <Paragraphs
+        showInstructions={showInstructions}
+        responseData={responseData}
+        isLoading={isLoading}
+      />
+      <RadialProgressBar responseData={responseData} isLoading={isLoading} />
+      <Alternatives responseData={responseData} isLoading={isLoading} />
       {console.log(responseData)}
     </>
   );
