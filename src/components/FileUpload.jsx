8cc5c7ee5c07be12376c8ae5efc8a69e9dca73dc
Himanshu Pathak <himanshupat95@gmail.com>
2024-04-06 13:26:42 +0530

    upload file seperated

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -3,6 +3,7 @@ import RadialProgressBar from "./RadialProgressBar";
 import Alternatives from "./Alternatives";
 import Paragraphs from "./Paragraphs";
 import DragAndDropArea from "./DragAndDropArea";
+import { uploadFile } from "../utils/uploadFile";
 
 const FileUpload = () => {
   const [highlight, setHighlight] = useState(false);
@@ -43,50 +44,11 @@ const FileUpload = () => {
 
     const [file] = e.target.files || e.dataTransfer.files;
 
-    uploadFile(file);
+    uploadFile(file, setPreview, setResponseData, setIsLoading);
     setFileUploaded(true);
     setShowInstructions(false);
     setIsLoading(true);
   };
-
-  function uploadFile(file) {
-    const reader = new FileReader();
-    reader.readAsBinaryString(file);
-
-    reader.onload = () => {
-      const fileRes = btoa(reader.result);
-      console.log(`data:image/jpg;base64,${fileRes}`);
-      setPreview(`data:image/jpg;base64,${fileRes}`);
-
-      const formData = new FormData();
-      console.log(file);
-      formData.append("file", file);
-
-      fetch("http://localhost:3000/upload", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setResponseData(data.data);
-        })
-        .catch((error) => {
-          setIsLoading(false);
-          console.error(error);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    };
-
-    reader.onerror = () => {
-      console.log("There is a problem while uploading...");
-    };
-  }
 
   return (
     <>
